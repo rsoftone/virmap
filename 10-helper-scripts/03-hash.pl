0f2272ca62051a45178bf99049f0eca42b82730e
David McFarlane <54093156+Prepultrue@users.noreply.github.com>
2019-11-29 17:46:20 +1100

    (WIP) Add 03-hash.pl which uses sorted GbAccList to improve speed

---
+++ 10-helper-scripts/03-hash.pl
@@ -0,0 +1,104 @@
+#!/usr/bin/env perl
+use strict;
+use English '-no_match_vars';
+use Boulder::Genbank;
+use List::Util qw[min max];
+
+my $input_fn = shift;
+my $genbFile = shift;
+
+my $input_fh;
+my $lookupval = "NONE";
+
+my $genbank_taxonomy_id;
+my $genbank_accession;
+my $genbank_version;
+
+open $input_fh, '<', $input_fn or die 'Could not open file: ', $OS_ERROR;
+
+
+sub next_accver {
+	my $line = <$input_fh>;
+	chomp $line;
+	return undef if !$line;
+	my ( $accession, $version, $gi ) = split /,/, $line, 3;
+	return "$accession.$version", $gi;
+}
+
+# binary_seek will gradually increase skip_amount to a max of 50MiB when it
+# undershoots the target accession, allowing skip_amount to be initially low
+# as it's common for the accessions of interest to be relatively clustered.
+sub binary_seek {
+	my $target      = $_[0];
+	my $init_pos    = $_[1];
+	my $skip_amount = $_[2];
+
+	# make the assumption that a skip_amount of < 5 is indicative of skipping
+	# less than a line ==> i.e. can't be found
+	return undef if ( $skip_amount < 5 );
+
+	my $seek_pos = $init_pos + $skip_amount;
+	seek $input_fh, $seek_pos, 0;
+	readline $input_fh;    # jump to the start of a line
+
+	my ( $accssession_ver, $gi ) = next_accver();
+
+	if ( $accssession_ver gt $target ) {
+		return binary_seek( $target, $init_pos, int( $skip_amount / 2 ) );
+	}
+	if ( $accssession_ver lt $target ) {
+		return binary_seek( $target, $seek_pos, min( $skip_amount * 2, 52428800 ) );
+	}
+	return $gi if ( $accssession_ver eq $target );
+
+	die "what?";
+}
+
+# simple driver around lookingup up gi from accession using binary_seek
+sub find_gi {
+	my $target = $_[0];
+
+	my $pos = tell $input_fh;
+	my ( $accssession_ver, $gi ) = next_accver();
+
+	return undef if ( $accssession_ver gt $target );
+	return $gi   if ( $accssession_ver eq $target );
+
+	return binary_seek( $target, $pos, 512 );
+}
+
+my $gb = new Boulder::Genbank(
+	-accessor => 'File',
+	-fetch    => $genbFile
+);
+
+
+if ($@) {
+	print " Error: $@ ";
+	exit(-1);
+}
+
+while ( my $s = $gb->get ) {
+	my $seq_object = $s->bioSeq;
+
+	if ( $s->Features->Source->Db_xref =~ m/taxon:(\d+)/ ) {
+		$genbank_taxonomy_id = $1;
+	}
+
+	$genbank_accession = $s->Accession;
+	$genbank_version   = $s->Version;
+
+	$lookupval = find_gi("$genbank_version");
+
+	# if (! defined($lookupval)) {
+	if ($@) {
+		$lookupval = "NOT FOUND";
+	}
+	print ">gi|", $lookupval, "|", "gb", "|", $genbank_version, "|", $seq_object->desc,
+	  "...;taxId=", $genbank_taxonomy_id, "\n";
+
+	print uc( $s->Sequence ), "\n";
+	print "\n";
+}
+
+close $input_fh or die 'Could not close file: ', $OS_ERROR;
---
+++ 10-helper-scripts/05-genbank-fasta-nucleo.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+###########################################################################
+#  Copyright 2019 University of New South Wales
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+###########################################################################
+#
+#PBS -l ncpus=1
+#PBS -l mem=1gb
+#PBS -l walltime=00:30:00
+#PBS -l wd
+#PBS -j oe
+#
+# 05-genbank-fasta-nucleo.sh
+#
+function usage() {
+    printf "\nUsage: ./05-genbank-fasta-nucleo.sh path/to/sorted/GbAccList path/to/genbank/divisions [threads]\n"
+}
+#
+# Assumes you have run the script in the previous section Part 3: Genbank division download
+#
+## Expected form of qsub:
+##
+## qsub -v SORTED_GB_ACC_LIST="${SORTED_GB_ACC_LIST}" -v GENBANKPATH="${GENBANKPATH}" 05-genbank-fasta-nucleo.sh
+##
+## Output directory for .fasta file: converted/nucleotide
+##
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [[ $# -eq 0 ]] && { [[ -z "${SORTED_GB_ACC_LIST:-}" ]] || [[ -z "${GENBANKPATH:-}" ]]; }; then
+    usage
+    exit 0
+fi
+
+SCRIPT_DIR="$(cd "$(dirname "$(readlink -e "${BASH_SOURCE[0]}")")" >/dev/null 2>&1 && pwd)"
+PREFPATH=converted/nucleotide
+mkdir -p "$PREFPATH"
+
+THREADS="${3:-${PBS_NCPUS:-${NCPUS:-$(nproc)}}}"
+
+if [[ -z "${SORTED_GB_ACC_LIST:-}" ]] || [[ -z "${GENBANKPATH:-}" ]]; then
+    SORTED_GB_ACC_LIST="$1"
+    GENBANKPATH="$2"
+fi
+
+function process() {
+    local div_file=$GENBANKPATH/$1
+    local out_file=$PREFPATH/$(echo "$1" | sed "s=.seq.gz$=.fasta=g")
+
+    if [[ -s "${out_file}" ]]; then
+        echo "Skipping $1 as it appears to have been processed already"
+        return 0
+    fi
+
+    set -e
+    perl "$SCRIPT_DIR/03-hash.pl" "$SORTED_GB_ACC_LIST" <(pigz -dc "$div_file") >"${out_file}.part"
+    mv "${out_file}.part" "${out_file}"
+}
+export -f process
+export PREFPATH GENBANKPATH SORTED_GB_ACC_LIST SCRIPT_DIR
+
+printf '%s\0' "$GENBANKPATH/"*.seq.gz | sort -zV |
+    xargs -0I'{}' basename '{}' |
+    parallel -j"$THREADS" -tI'{}' process {}
